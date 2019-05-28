04992ac4e5bd240b427cdf4b2c8f99f18e687d68
KeisukeMurata4210 <keisuke.murata4210@gmail.com>
2019-05-28 18:48:04 +0900

    12から

--- chapter2/section_10.rb
+++ chapter2/section_10.rb
@@ -1,7 +1,67 @@
+require('csv')
+class AnnualWeather
+  def initialize (file_name)
+    @readings = []
 
+    CSV.foreach(file_name, headers: true) do |row| # ハッシュを使うと、どのキーがCSVのどの列に対応するか、都度ここに戻って確認する必要が出てしまう
+      @readings << {
+        :date => Date.parse(row[2]),
+        :high => row[10].to_f,
+        :low => row[11].to_f
+      }
+    end
+  end
+
+  def mean
+    return 0.0 if @readings.size.zero?
+
+    total = @readings.reduce(0.0) do |sum, reading|
+      sum + (reading[:high] + reading[:low]) / 2.0 # <= この部分
+    end
+
+    total / @readings.size.to_f
+  end
+end
+
+
+
+class AnnualWeather
+  # 観測データを保持する新しいStructクラスを作る
+  Reading = Struct.new(:date, :high, :low)
+
+  def initialize (file_name)
+    @readings = []
+
+    CSV.foreach(file_name, headers: true) do |row|
+      @readings << Reading.new(Date.parse(row[2]), # 新しいクラスReadingのオブジェクトがどのメソッドに応答するかはっきりする
+                               row[10].to_f,
+                               row[11].to_f)
+    end
+  end
+
+  def mean
+    return 0.0 if @readings.size.zero?
+
+    total = @readings.reduce(0.0) do |sum, reading|
+      sum + (reading.high + reading.low) / 2.0  # タイプミスをした場合、ハッシュのキーはnilが返されるだけだが、Structを使うとNoMethodErrorが発生する
+    end
+
+    total / @readings.size.to_f
+  end
+end
+
+# Structは自身にメソッドを定義できる。=> 各月ごとのmeanメソッドを作れる
+Reading = Struct.new(:date, :high, :low) do
+  def mean
+    (high + low) / 2.0
+  end
+end
+# インターフェース上の問題は解決したため、@readingsをattr_accessorで公開もできる
 
 
 =begin
 ＜この項目で気づいたこと・学んだこと＞
-
+・一定の規則で並んだデータは、HashじゃなくてStructを使う。
+・一つ一つのデータについて、必要なデータや処理をStructのインスタンス変数とメソッドとして隠蔽する。かなりスッキリする。
+・Structにメソッドの定義もできてかなりスッキリする！
 =end
---
+++ chapter2/section_11.rb
@@ -0,0 +1,68 @@
+class Binding
+  def initialize (style, options = {})
+    @style = style
+    @options = options
+  end
+end
+# 何がまずいか。Bindingはコアライブラリにあるクラスで、それを書き換えてしまったことになる。
+# 名前空間を作って区別する
+module Notebooks  # 名前空間はディレクトリ構成と対応させる。今回は Notebooks/Binding.rb ということ
+  class Binding
+    # ...
+  end
+end
+style = Notebooks::Binding.new
+
+# すでに名前空間ができている場合などの短縮形は
+class Notebooks::Binding
+  # ...
+end
+# ただし名前空間ができてなければNameErrorが発生する。
+
+
+
+module SuperDumbCrypto
+  KEY = "password123"
+
+  class Encrypt
+    def initialize (key=KEY) # これはフルクラスパスなしで呼び出せる
+      # ...
+    end
+  end
+end
+
+module SuperDumbCrypto
+  KEY = "password123"
+end
+
+class Encrypt
+  def initialize (key=KEY) # これはNameErrorが発生する。レキシカルスコープ内・継承階層経由のどちらもKEYが見つからないから
+    # ...
+  end
+end
+# これなら動く
+class SuperDumbCrypto::Encrypt
+  def initialize (key=SuperDumbCrypto::KEY) # トップレベル定数はObjectクラスに格納されている。継承階層を辿っていき、ObjectからKEYを見つけ出す。
+    # ...
+  end
+end
+
+
+
+module Cluster
+  class Array
+    def initialize (n)
+      # 間違ったArray。SystemStackErrorが起こる。このArrayはCluster::Arrayを意味するから
+      @disks = Array.new(n) {|i| "disk#{i}"}
+      # こうすればOK。Object::Arrayは::Arrayに省略できる。
+      @disks = ::Array.new(n) {|i| "disk#{i}"}
+    end
+  end
+end
+
+=begin
+＜この項目で気づいたこと・学んだこと＞
+・モジュールで名前空間を作ろう。
+・名前空間はディレクトリ構成と同じにする。
+・トップレベルの定数はObjectクラスに格納されている。だから明示的にトップクラスの定数やクラスを使うときは::Arrayなどと書く。
+=end
