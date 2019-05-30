760a9077ef3de4ee6f34bd4b43402a0a70736097
KeisukeMurata4210 <keisuke.murata4210@gmail.com>
2019-05-30 20:08:08 +0900

    頭が疲れたよー

---
+++ chapter4/section_23.rb
@@ -0,0 +1,64 @@
+begin
+  task.perform
+rescue => e
+  logger.error("task failed: #{e}")
+  # 例外を飲み込み、タスクを中止する。
+end
+# ほとんど全てのエラーを飲み込んでしまう。AgumentError, LocalJumpErrorのようなコードミスも含めて。
+
+# これはよくない。
+begin
+  task.perform
+rescue AgumentError, LocalJumpError,
+       NoMethodError, ZeroDivisionError
+  raise # rescue内でraiseを呼び出すと、呼び出し元に戻る。そうしないとrescue内の処理を実行するだけ。
+rescue => e
+  logger.error("task failed: #{e}")
+  # 例外を飲み込み、タスクを中止する。
+end
+# StandardErrorを先に捕まえてしまうなど、先に範囲の広い例外をキャッチしてしまうかもしれない。
+# 意図が伝わりにくい。後で消されるかもしれない。
+
+# 自作の例外クラスを作るメリットがここではっきりする
+begin
+  task.perform
+rescue NetworkConnectionError => e
+  # 再試行のロジック
+rescue InvalidRecordError => e
+  # サポートスタッフにレコードを送る
+end
+
+# 例外全般を捕まえる時の注意点
+begin
+  task.perform
+rescue NetworkConnectionError => e
+  # 再試行のロジック
+rescue InvalidRecordError => e
+  # サポートスタッフにレコードを送る
+rescue => e
+  service.record(e)
+  raise
+ensure
+  # ...
+end
+# service.record(e)で例外が発生すると、元の例外情報を捨てて、rescue節のスコープを抜けて、新しい例外処理が始まってしまう。
+
+# 元の例外を引数として受け取る専用メソッドを作って、raiseでその例外を送れば解消する
+def send_to_support_staff (e)
+  begin
+    service.record(e)
+  rescue
+    raise(e)
+  end
+end
+# 元の例外の方が大切、という前提での処理
+
+
+
+=begin
+＜この項目で気づいたこと・学んだこと＞
+・修復の仕方がわかっている例外だけをrescueする。
+・より限定されたものからrescueする。
+・StandardErrorのような汎用例外をrescueするときは、ensureを使いたいときだと考えてみる。
+・rescueで例外が発生すると、新しい例外が現在の例外を押しのけてしまう。専用メソッドで隔離して、そこでraiseで例外オブジェクトeを送る
+=end
