d8ffdfe831d1d55ecb64122df2b574574e0e8b05
KeisukeMurata4210 <keisuke.murata4210@gmail.com>
2019-07-04 18:46:49 +0900

    テストに入った

--- chapter5/section_35.rb
+++ chapter5/section_35.rb
@@ -19,8 +19,29 @@ class C
   end
 end
 
+C.ancestors # => [C, B, A, Object, Kernel, BasicObject]
+# 先入れ後出しだから、上でインクルードしたAが後になる
+C.new.who_am_i? # => "C#who_am_i?"
+
+# prependを使う
+class C
+  prepend(A)
+  prepend(B)
+
+  def who_am_i?
+    "C#who_am_i?"
+  end
+end
+C.ancestors # => [B, A, C, Object, Kernel, BasicObject]
+C.new.who_am_i? # => "B#who_am_i?"
+# prependを使うと、レシーバの前にメソッドを挿入する。
+
+
+
 
 =begin
 ＜この項目で気づいたこと・学んだこと＞
-
+・includeはスーパークラスの前だが、prependはレシーバの前にメソッドを挿入する。
+・最後にprependしたものが一番最初に呼ばれる
+・それ以外の使い方はincludeと同じ。prependedフックもある。
 =end
---
+++ chapter6/section_36/version_test.rb
@@ -0,0 +1,33 @@
+require('minitest/autorun')
+require('./version.rb')
+
+class VersionTest < Minitest::Test
+  def test_major_number
+    v = Version.new("2.1.3")
+    assert(v.major == 2, "major should be 2")
+  end
+
+  def test_can_parse_version_string
+    v = Version.new("2.1.3")
+    assert_equal(2, v.major, "major")
+    assert_equal(1, v.minor, "minor")
+    assert_equal(3, v.patch, "patch")
+  end
+
+  # setup
+  def setup
+    @v1 = Version.new("2.1.1")
+    @v2 = Version.new("2.3.0")
+  end
+
+  def test_version_compare
+    refute_equal(@v1, @v2) # 比較は==演算子
+    assert_operator(@v1, :<, @v2)
+  end
+end
+
+
+=begin
+＜この項目で気づいたこと・学んだこと＞
+
+=end
